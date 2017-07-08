4ba86f3b21cb013565bd059ba3c9af9ae6d7ba75
Manuel Domke <domke@denic.de>
2017-07-08 14:58:18 +0200

    fixed DHCP handling

--- webServer.lua
+++ webServer.lua
@@ -1,5 +1,14 @@
 local sendHttpResponse = require("sendHttpResponse")
 
+     
+local unescape = function (s)
+   s = string.gsub(s, "+", " ")
+   s = string.gsub(s, "%%(%x%x)", function (h)
+         return string.char(tonumber(h, 16))
+      end)
+   return s
+end
+
 -- Small footprint webserver to serve files from flash
 local function webServer(connection, request)
     local match = request:match("GET / ")
@@ -10,7 +19,7 @@ local function webServer(connection, request)
     else
         match = request:match("/(%w*%.%w*)")
     end
-        
+   
         local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
            if(method == nil)then
                _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP")
@@ -21,7 +30,9 @@ local function webServer(connection, request)
                    _GET[k] = unescape(v)
                end
            end
-             
+           
+         --  print(_GET.ap)
+         --  print(_GET.key)
            if (_GET.key ~= nil and _GET.ap ~= nil) then
               connection:send("Saving wifi confiuguration..")
               file.open("wifiSettings.lua", "w")
@@ -29,17 +40,36 @@ local function webServer(connection, request)
 
               file.writeline('wifiSettings.ssid = "' .. _GET.ap .. '"')
               file.writeline('wifiSettings.key = "' .. _GET.key .. '"')
-              file.writeline('wifiSettings.ip = "' .. _GET.ip .. '"')
-              file.writeline('wifiSettings.netmask = "' .. _GET.nm .. '"')
-              file.writeline('wifiSettings.gateway = "' .. _GET.gw .. '"')  
+              if (_GET.ip ~= nil) then
+                file.writeline('wifiSettings.ip = "' .. _GET.ip .. '"')
+              end
+              if (_GET.nm ~= nil) then
+                file.writeline('wifiSettings.netmask = "' .. _GET.nm .. '"')
+              end
+              if (_GET.gw ~= nil) then
+                file.writeline('wifiSettings.gateway = "' .. _GET.gw .. '"')  
+              end
               file.writeline('return wifiSettings')
               file.close()
               node.compile("wifiSettings.lua")
               file.remove("wifiSettings.lua")
-              client:send(buf)
+              
+              tmr.delay(1000 * 1000)
               node.restart()
            end
-           
+
+           if (_GET.pwmFreq ~= nil) then
+              connection:send("Saving PWM configuration..")
+              file.open("pwmSettings.lua", "w")
+              file.writeline('local pwmSettings = {}')
+              file.writeline('pwmSettings.pwmFreq = "' .. _GET.pwmFreq .. '"')
+              file.writeline('return pwmSettings')
+              file.close()
+              node.compile("pwmSettings.lua")
+              file.remove("pwmSettings.lua")
+              node.restart();
+           end
+
     if match then
         local fileSize = file.list()[match]
 
