66338427e85a6a70d135cd27b4a7246ad772eb43
Manuel Domke <domke@denic.de>
2017-07-08 14:58:46 +0200

    fixed DHCP handling

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
 wifiSettings = require("wifiSettings")
 
 autoWifi  = require("autoWifi")
-autoWifi.setup(wifiSettings.ssid, wifiSettings.key, {ip=wifiSettings.ip, netmask=wifiSettings.netmask, gateway=wifiSettings.gateway})
+if (wifiSettings.ip ~= nil and wifiSettings.netmask ~= nil and wifiSettings.gateway ~= nil) then
+    ip_config= {ip=wifiSettings.ip, netmask=wifiSettings.netmask, gateway=wifiSettings.gateway}
+else
+    ip_config=nil
+end    
+autoWifi.setup(wifiSettings.ssid, wifiSettings.key, ip_config)
 
 updateServer = require("updateServer")
 updateServer.start()
